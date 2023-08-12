d59768469da558d237d0c04e6f02cccdd01285c3
BuildTools <unconfigured@null.spigotmc.org>
2023-08-12 12:56:03 +0200

    Start

--- src/main/java/me/sunka/flycommand/commands/Crafting.java
+++ src/main/java/me/sunka/flycommand/commands/Crafting.java
@@ -1,7 +1,5 @@
 package me.sunka.flycommand.commands;
 
-import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
